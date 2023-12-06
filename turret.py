8c180154175efd8ac6b730256580ead4d2bd4178
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-12-06 09:59:52 -0300

    troca o nome do arquivo e cria classe Torre

---
+++ turret.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Turret(pg.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect
+        self.rect.center = pos
