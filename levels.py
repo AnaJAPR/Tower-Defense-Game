b67da307dafda99e37d0e839ec8c4f806e656ce6
paulocgr9 <126474247+paulocgr9@users.noreply.github.com>
2023-12-09 18:42:48 -0300

    Adicionado documentação para levels.py

--- levels.py
+++ levels.py
@@ -4,7 +4,54 @@ from constants import HEALTH, MONEY
 from game.load_enemy import ENEMY_SPAWN_DATA
 
 class Level():
+    """
+    Represents the game level with its mechanics.
+
+    Parameters
+    ---------- 
+    map_image : str
+        The file path to the file where the image is.
+    waypoints : list
+        A list of tuples representing the x and y coordinates of the points the enemies
+        must walk through.
+    
+    Attributes
+    ----------
+    level : int
+        The current level of the game.
+    spawned_enemies : int
+        Amount of enemies that have been spawned.
+    enemy_list : list
+        List of the enemies that have been spawned.
+    __map_image : pygame.surface
+        An image of the map loaded using pygame.
+    _health : int
+        The player's health.
+    __initial_money : int
+        The initial amount of money the player has.
+    _money : int
+        The current amount of money the player has.
+    _waypoints : list
+        A list of points defining the path that the enemies must follow.
+    killed_enemies : int
+        The amount of enemies that were killed during the level.
+
+    Example
+    -------
+    level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+    """
     def __init__(self, map_image:str, waypoints:list):
+        """
+        Initialize a new instance of the Level class.   
+        
+        Parameters
+        ---------- 
+        map_image : str
+            The file path to the file where the image is.
+        waypoints : list
+            A list of tuples representing the x and y coordinates of the points the enemies
+            must walk through.
+        """
         self.level = 1
         self.spawned_enemies = 0
         self.enemy_list = []
@@ -16,32 +63,138 @@ class Level():
         self.killed_enemies = 0
 
     def transform_image_proportions(self, width:int, height:int):
+        """
+        Transform the image proportions to a specified width and height using Pygame's
+        transform.scale function.
+
+        Parameters
+        ---------- 
+        width: int
+            The desired width of the image.
+        height: int
+            The desired height of the image.
+
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        level.transform_image_proportions(69, 42)
+        """
         self.__map_image = pygame.transform.scale(self.__map_image, (width, height))
 
     def draw_map(self, surface):
+        """
+        Draw the map on the specified surface.
+
+        Parameters
+        ---------- 
+        surface : pygame.surface.Surface
+            The surface on which the map will be drawn.
+        
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        level.draw_map(screen)
+        """
         surface.blit(self.__map_image, (0, 0))
 
     @property
     def initial_money(self):
+        """
+        Get the initial amount of money the player has.
+        
+        Returns
+        -------
+        int
+            The initial amount of money the player has
+        
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        initial_money = level.initial_money
+        """
         return self.__initial_money
     
     @property
     def money(self):
+        """
+        Get the current amount of money the player has.
+        
+        Returns
+        -------
+        int
+            The current amount of money the player has
+        
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        initial_money = level.money
+        """
         return self._money
 
     @money.setter
     def money(self, money):
+        """
+        Set the current amount of money the player has.
+
+        Parameters
+        ----------
+        money : int
+            The updated amount of money of the player
+
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        level.money = 500
+        """
         self._money = money
 
     @property
     def health(self):
+        """
+        Get the current health the player has.
+        
+        Returns
+        -------
+        int
+            The current health the player has
+        
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        health = level.health
+        """
         return self._health
 
     @health.setter
     def health(self, health):
+        """
+        Set the current health the player has.
+
+        Parameters
+        ----------
+        health : int
+            The updated health of the player
+
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        level.health = 25
+        """
         self._health = health
 
     def spawn_enemies(self):
+        """
+        Spawn enemies on the map according to some level attributes.
+
+        Parameters
+        ---------- 
+        None
+
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        level.spawn_enemies()
+        """
         enemies = ENEMY_SPAWN_DATA[self.level - 1]
         for enemy_type in enemies:
             enemies_to_spawn = enemies[enemy_type]
@@ -51,12 +204,24 @@ class Level():
         random.shuffle(self.enemy_list)
 
     def clear_level_data(self):
+        """
+        Reset all the game mechanics to their initial status.
+
+        Parameters
+        ---------- 
+        None
+        
+        Example
+        -------
+        level = Level("assets/maps/map_1.png", [(0, 0), (40, 60), (100, 100)])
+        level.clear_level_data()
+        """
         self.spawned_enemies = 0
         self.enemy_list = []
         self._health = HEALTH
         self._money =  self.__initial_money
         self.killed_enemies = 0
 
-    def loose_game(self):
+    def lose_game(self):
         if self._health <= 0:
             pass
