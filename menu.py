c2dd96e058f3c507377785191cad7bea121de691
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-12-09 10:47:38 -0300

    adiciona proposta de modificação da logica do codigo menu

--- menu.py
+++ menu.py
@@ -1,6 +1,149 @@
 import pygame
 import sys
 import subprocess
+import os
+
+class Button:
+    def __init__(self, image, pos, text_input, base_color, hovering_color):
+        # Initialize a Button object with its position, size, text, and associated action
+        self.image = image
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = self.font.SysFont(None, 36)
+        self.base_color, self.hovering_color = (186, 147, 216), (160, 110, 200)
+        self.text_input = text_input
+        self.font_size = 36
+        self.font = pygame.font.SysFont(None, self.font_size)
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.rect)
+        
+    def checkForInput(self, position):
+        return self.rect.collidepoint(position)
+    
+    def changeColor(self, position):
+        if self.rect.collidepoint(position):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.base_color)
+
+class Menu:
+    def __init__(self):
+        pygame.init()
+        self.SCREEN = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("Tower Defense Menu")
+        self.BG = pygame.image.load("assets/background_menu/menu_image.jpeg")
+        self.run()
+    
+    def play(self):
+        while True:
+            PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+            self.SCREEN.fill((0,0,0))
+
+            PLAY_TEXT = pygame.font.Font(none, 36).render("This is the PLAY screen.", True, (255, 255, 255))
+            PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+            SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+            PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", base_color="White", hovering_color="Green")
+
+            PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+            PLAY_BACK.update(self.SCREEN)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                        self.main_menu()
+
+            pygame.display.update()
+
+    def options(self):
+        while True:
+            OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+            self.SCREEN.fill((255,255,255))
+
+            OPTIONS_TEXT = pygame.font.Font(None, 36).render("This is the OPTIONS screen.", True, "Black")
+            OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+            self.SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+            OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", base_color="Black", hovering_color="Green")
+
+            OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+            OPTIONS_BACK.update(self.SCREEN)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                        self.main_menu()
+
+            pygame.display.update()
+
+    def main_menu(self):
+        while True:
+            self.SCREEN.blit(self.BG, (0, 0))
+
+            MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+            MENU_TEXT = pygame.font.Font(None, 36).render("MAIN MENU", True, "#b68f40")
+            MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+            PLAY_BUTTON = Button(image=pygame.image.load("assets/buttons_images/Play Rect.png"), pos=(640, 250), 
+                            text_input="PLAY", base_color="#d7fcd4", hovering_color="White")
+            OPTIONS_BUTTON = Button(image=pygame.image.load("assets/buttons_images/Options Rect.png"), pos=(640, 400), 
+                            text_input="OPTIONS", base_color="#d7fcd4", hovering_color="White")
+            QUIT_BUTTON = Button(image=pygame.image.load("assets/buttons_images/Quit Rect.png"), pos=(640, 550), 
+                            text_input="QUIT", base_color="#d7fcd4", hovering_color="White")
+
+            self.SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+            for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+                button.changeColor(MENU_MOUSE_POS)
+                button.update(self.SCREEN)
+        
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        self.play()
+                    if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        self.options()
+                    if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        pygame.quit()
+                        sys.exit()
+
+            pygame.display.update()
+            
+    def run(self):
+        self.main_menu()
+
+if __name__ == "__main__":
+    menu = Menu()
+
+
+
+
+
+'''
+import pygame
+import sys
+import subprocess
 
 class Button:
     def __init__(self, text, x, y, width, height, action=None):
@@ -57,12 +200,12 @@ class Menu:
                 pygame.quit()
                 sys.exit()
 
-            '''
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    print("Tecla Escape pressionada")
-                    return 
-            '''
+            
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_ESCAPE:
+            #         print("Tecla Escape pressionada")
+            #         return 
+            
 
             if event.type == pygame.VIDEORESIZE:
                 self.handle_resize(event)
@@ -217,3 +360,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
