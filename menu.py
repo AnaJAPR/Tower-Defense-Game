3deedb5f4134ca7208cfed23d358316ee4f4b5a6
Ana Júlia Amaro Pereira Rocha <B51991@fgv.edu.br>
2023-12-09 21:54:48 -0300

    adiciona doctring ao menu

--- menu.py
+++ menu.py
@@ -3,11 +3,53 @@ import sys
 import subprocess
 
 class Button:
-    def __init__(self, image, pos, text_input, base_color, hovering_color, min_size=(100, 50)):
-        '''
-        
-        
-        '''
+    """
+    Represents an interactive button in a graphical interface.
+
+    Parameters
+    ----------
+    image : pygame.surface.Surface
+        The image associated with the button. Can be None if the button has no image.
+    pos : tuple
+        The initial position of the button as a tuple (x, y).
+    text_input : str
+        The text to be displayed on the button.
+    base_color : tuple or str
+        The base color of the button when not hovered, specified as an RGB tuple or as a string with the name of a color.
+    hovering_color : tuple
+        The base color of the button when hovered over, specified as an RGB tuple or as a string with the name of a color.
+    min_size : tuple, optional
+        Minimum size of the button, specified as (width, height). Default is (100, 50).
+
+    Attributes
+    ----------
+    image : pygame.surface.Surface
+        An image of the button loaded using pygame.
+    x_pos : int
+        x coordinate of the top-left corner of the button.
+    y_pos : int
+        y coordinate of the top-left corner of the button.
+    base_color : tuple or str
+        The base color of the button when not hovered, specified as an RGB tuple or as a string with the name of a color.
+    hovering_color : tuple or str
+        The base color of the button when hovered over, specified as an RGB tuple or as a string with the name of a color.
+    text_input : str
+        The text to be displayed on the button.
+    font_size : int
+        Font size for the button text.
+    font : pygame.font.Font
+        Font object for rendering text.
+    text : pygame.surface.Surface
+        Rendered text surface.
+    rect : pygame.rect.Rect
+        Rectangular area where the button is drawn.
+
+    Example
+    -------
+    START_GAME_BUTTON = Button(image=None, pos=(560,25), text_input="START GAME", base_color=(94, 88, 49), hovering_color=(207, 126, 18))
+    """
+    
+    def __init__(self, image, pos:tuple, text_input, base_color, hovering_color, min_size=(100, 50)):
         self.image = image
         self.x_pos = pos[0]
         self.y_pos = pos[1]
@@ -21,21 +63,103 @@ class Button:
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 
     def update(self, screen):
+        """
+        Updates the representation of the button on the screen.
+
+        Parameters
+        ----------
+        screen : pygame.surface.Surface
+            The Pygame screen where the button will be rendered.
+
+        Example
+        -------
+        START_GAME_BUTTON = Button(image=None, pos=(560,25), text_input="START GAME", base_color=(94, 88, 49), hovering_color=(207, 126, 18))
+        START_GAME_BUTTON.update(pygame.display.get_surface())
+        """
+        
         if self.image is not None:
             screen.blit(self.image, self.rect)
         screen.blit(self.text, self.rect)
         
     def checkForInput(self, position):
+        """
+        Checks if the given position is over the button.
+
+        Parameters
+        ----------
+        position : tuple
+            The position to be checked as a tuple (x, y).
+
+        Returns
+        -------
+        bool
+            True if the position is over the button, False otherwise.
+
+        Example
+        -------
+        START_GAME_BUTTON = Button(image=None, pos=(560,25), text_input="START GAME", base_color=(94, 88, 49), hovering_color=(207, 126, 18))
+        is_over = START_GAME_BUTTON.checkForInput((520, 520))
+        """
+        
         return self.rect.collidepoint(position)
     
     def changeColor(self, position):
+        """
+        Changes the color of the button based on the mouse position.
+
+        Parameters
+        ----------
+        position : tuple
+            The mouse position as a tuple (x, y).
+        
+        Returns
+        -------
+        None
+
+        Example
+        -------
+        START_GAME_BUTTON = Button(image=None, pos=(560,25), text_input="START GAME", base_color=(94, 88, 49), hovering_color=(207, 126, 18))
+        START_GAME_BUTTON.changeColor((520, 520))
+        """
+        
         if self.rect.collidepoint(position):
             self.text = self.font.render(self.text_input, True, self.hovering_color)
         else:
             self.text = self.font.render(self.text_input, True, self.base_color)
 
 class Menu:
+    """
+    Represents a graphical interface for the game menu.
+
+    This class initializes the game menu, defines its buttons, and specifies actions associated with them.
+
+    Attributes
+    ----------
+    SOUND_TOGGLE : Button
+        Button for toggling sound.
+    sound_enabled : bool
+        Indicates whether the sound is enabled or not.
+    BG : pygame.surface.Surface
+        Background image of the menu.
+    SCREEN : pygame.surface.Surface
+        Pygame screen for displaying the menu.
+    game_running : bool
+        Represents whether the game is currently running (True) or not (False).
+
+    Example
+    -------
+    menu = Menu()
+    menu.run()
+    """
+    
     def __init__(self):
+        """
+        Initialize the Menu.
+
+        Example
+        -------
+        menu = Menu()
+        """
         pygame.init()
         pygame.mixer.init()
         pygame.mixer.music.load("assets/menu_audios_images/menu_audio.mp3")
@@ -54,6 +178,14 @@ class Menu:
         self.run()
     
     def play(self):
+        """
+        Display the screen for starting the game.
+
+        Example
+        -------
+        menu.play()
+        """
+        
         while True:
             PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
@@ -88,6 +220,14 @@ class Menu:
             pygame.display.update()
 
     def sobre_o_jogo(self):
+        """
+        Display the screen for information about the game.
+
+        Example
+        -------
+        menu.sobre_o_jogo()
+        """
+
         while True:
             ABOUT_THE_GAME_MOUSE_POS = pygame.mouse.get_pos()
 
@@ -114,6 +254,14 @@ class Menu:
             pygame.display.update()
             
     def confirm_quit(self):
+        """
+        Display the screen to confirm quitting the game.
+
+        Example
+        -------
+        menu.confirm_quit()
+        """
+        
         while True:
             self.SCREEN.fill((254,217,139))
 
@@ -146,6 +294,14 @@ class Menu:
                         return
 
     def start_game(self):
+        """
+        Start the game in a new process.
+
+        Example
+        -------
+        menu.start_game()
+        """
+
         print("Starting the game...")
 
         python_command = "python"
@@ -156,6 +312,14 @@ class Menu:
         sys.exit()
 
     def main_menu(self):
+        """
+        Display the main menu.
+
+        Example
+        -------
+        menu.main_menu()
+        """        
+
         while True:
             self.SCREEN.blit(self.BG, (0, 0))
 
@@ -191,6 +355,14 @@ class Menu:
             pygame.display.update()
             
     def toggle_sound(self):
+        """
+        Toggle the sound between on and off.
+
+        Example
+        -------
+        menu.toggle_sound()
+        """
+        
         if self.sound_enabled:
             pygame.mixer.music.pause()
             self.SOUND_TOGGLE.image = pygame.transform.scale(pygame.image.load("assets/buttons/sound_off.jpg"), (40,40))
@@ -200,6 +372,14 @@ class Menu:
         self.sound_enabled = not self.sound_enabled
             
     def run(self):
+        """
+        Run the main menu.
+
+        Example
+        -------
+        menu.run()
+        """
+
         self.main_menu()
 
 if __name__ == "__main__":
